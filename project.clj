151ca0ef63ab948ccc495e998013361615598549
Danny Guinther <dguinther@seismic.com>
2018-08-01 12:04:01 -0400

    Swap org info to deploy fork to clojars

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject lein-dynamic-version "0.1.2-SNAPSHOT"
+(defproject co.zensight/lein-dynamic-version "0.1.2-SNAPSHOT"
   :description "Populate your leiningen project version from external sources"
-  :url "https://github.com/pelletier/lein-dynamic-version"
+  :url "https://github.com/Zensight/lein-dynamic-version"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
-  :repositories [["releases" {:url "https://clojars.org/repo" :creds :gpg}]]
+  :deploy-repositories [["releases" :clojars]]
   :profiles {:clojure-1.8.0 {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :clojure-dev {:dependencies [[org.clojure/clojure "1.9.0-alpha14"]]}
              :test {:resource-paths ["test/resources"]}})
